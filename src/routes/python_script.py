47b6a2467a2dab37dafb5f2d3606e59da0f10626
Ashwini-Darekar <ashwinidarekar3096@gmail.com>
2023-08-23 10:27:26 +0530

    #1 update postgres dump

--- src/routes/python_script.py
+++ src/routes/python_script.py
@@ -10,8 +10,7 @@ import influxdb_client
 from starlette.middleware.cors import CORSMiddleware
 from os import getenv
 from datetime import datetime
-import psycopg2
-
+import psycopg2.extras
 
 router = APIRouter()
 
@@ -44,23 +43,24 @@ def move_to_postgres(df):
     # Create an SQLAlchemy engine
     engine = create_engine(f'postgresql://{username}:{password}@10.129.2.205:5432/{database}')
 
-    # Read the DataFrame (assuming you have all_dataframes and 'df_Vizianagaram' inside it)
-
-    # df = get_all_data()
     # Store the DataFrame in the PostgreSQL table
     df.to_sql(table_name, con=engine, if_exists="append", index=False)
 
-    # Count the rows in the table
     try:
-        connection = engine.connect()
-        result = connection.execute(f'''SELECT * FROM "{table_name}"''')
-        print(f"Number of rows in the table: {result}")
-        for rslt in result:
-            print(rslt)
+        rdb = engine.raw_connection()
+        cursor = rdb.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        # Execute the SQL query
+        query = f'''SELECT * FROM "{table_name}"'''
+        cursor.execute(query)
+        result = cursor.fetchall()
+        for row in result:
+            print(row)
+        return result
     except Exception as e:
         print("Error:", e)
     finally:
-        connection.close()
+        # Close the database connection
+        rdb.close()
 
     # Close the database connection
     engine.dispose()
@@ -68,23 +68,22 @@ def move_to_postgres(df):
 
 @router.get("/data")
 def get_all_data(
-        # data: GetInputData
 ):
     mydata = []
     try:
         client = get_influx2_client()
-        start_time = datetime.now().date() - timedelta(days=7)
-        end_time = datetime.now().date()
+        start_time = datetime.now().date() - timedelta(days=9)
+        end_time = datetime.now().date() - timedelta(days=2)
         q = f'''
 
           from(bucket: "data")
           |> range(start: {start_time.strftime("%Y-%m-%dT00:00:00Z")} , stop: {end_time.strftime("%Y-%m-%dT23:59:59Z")})
           |> filter(fn: (r) => r["_measurement"] == "energy")
-          |> filter(fn: (r) => r["panel_no"] == "80660001_1")
+          |> filter(fn: (r) => r["panel_no"] == "80660037_1")
           |> filter(fn: (r) => r["device_code"] == "MOD02")
           |> filter(fn: (r) => r["zone"] == "KWH")
           |> timeShift(duration: 330m, columns: [ "_time"])
-          |> aggregateWindow(every: 1d, fn: last, createEmpty: false)
+          |> aggregateWindow(every:10m, fn: last, createEmpty: false)
           |> difference(nonNegative: false, columns: ["_value"])
 
 
@@ -97,9 +96,9 @@ def get_all_data(
             for records in x:
                 mydata.append(records.values)
         df = pd.DataFrame(mydata)
-        move_to_postgres(df)
-        print(df)
-        return df
+        res = move_to_postgres(df)
+        # print(df)
+        return res
     except Exception as e:
         # logger.critical(f'{e}')
         print("Exception is", e)
